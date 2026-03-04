724050b172ba5f61ea703d06f461532675ec30b0
manikumar <manikumar934@gmail.com>
2026-03-04 16:12:27 +0530

    added new s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mani-terraform-state-2026"
+    bucket         = "mani-state-2026"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
