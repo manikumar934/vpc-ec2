fb6085b0c58646ae0def679e37a24c0aa8cf028b
manikumar <manikumar934@gmail.com>
2026-03-04 06:14:59 +0530

    s3bucketname

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "mani-terraform-state-2026"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
